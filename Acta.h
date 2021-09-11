8b3e5d0fc4a4728a021df2a1f703080967323f8d
Aguado <juanjo.aguado2002@gmail.com>
2021-09-11 16:03:56 -0500

    subi el .h del acta y calificacion

---
+++ Acta.h
@@ -0,0 +1,35 @@
+//#ifndef	ACTA_H
+//Guardas
+//#define ACTA_H
+
+#include <iostream>
+#include <stdlib.h>
+#include "Calificacion.h"
+
+// Inclusion de librerias
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Acta{
+private: //Atributos
+	string fecha;
+	int numActa;
+	string nombreEstudiante;
+	string nombreTrabajo;
+	string nombreDirector;
+    string nombreCoDirector;
+    string nombreJuradoUno;
+    string nombreJuradoDos;
+    Calificacion calificaciones;
+    string comentariosGenerales;
+    estado;
+    float notaFinal;
+
+public:           //Metodos
+	Acta(); //Constructor
+	// Gets and sets
+    
+};
+//#endif /* !ACTA_H */
---
+++ Calificacion.h
@@ -0,0 +1,26 @@
+//#ifndef	CALIFICACION_H
+//Guardas
+//#define CALIFICACION_H
+
+#include <iostream>
+#include <stdlib.h>
+
+// Inclusion de librerias
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Calificacion {
+private: //Atributos
+	int id;
+	float calificacionJuradoUno;
+	float calificacionJuradoDos;
+	string comentarios;
+
+public:           //Metodos
+	Calificacion(); //Constructor
+	// Gets and sets
+	
+};
+//#endif /* !CALIFICACION_H */
