19c2d37c4da3830142c9b0e136a84d136285882d
Jvalencia330 <85379917+Jvalencia330@users.noreply.github.com>
2021-09-12 18:01:38 -0500

    Update View.cpp

---
+++ View.cpp
@@ -0,0 +1,21 @@
+#include "View.h"
+#include "Acta.h"
+
+View::View()
+{
+}
+
+void View::mostrarMenu()
+{
+    int opcion = -1;
+    do
+    {
+        // TODO completar
+        switch (opcion)
+        {
+            case 0:
+                break;
+        }
+
+    } while (opcion != 0);
+}
