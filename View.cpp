ab74d4a5505456095e49f6efd55375067b5a74bb
Jvalencia330 <85379917+Jvalencia330@users.noreply.github.com>
2021-09-13 21:57:58 -0500

    Menu View
    Adicion del esqueleto para el menu del view, inclusion de enum en criterio y

--- View.cpp
+++ View.cpp
@@ -5,15 +5,76 @@ View::View(){
 }
 
 void View::mostrarMenu(){
-    int opcion = -1;
+    int opcionMenuPrincipal = -1, opcionMenuSecundario = -1;
+    cout << "Menu\n";
+    cout << "Digite la opcion segun su tipo de usuario.\n";
+    cout << "1. Asistente de posgrado. \n2. Director(a). \n3. Jurado(a). \nOPC: ";
+    cin >> opcionMenuPrincipal;
     do
     {
-        // TODO completar
-        switch (opcion)
+        // Menu principal para escoger el rol
+        switch (opcionMenuPrincipal)
         {
             case 0:
                 break;
-        }
+            case 1:
+                do
+                {   //Menu secundario con las funcionalidades para ese rol
+                    cout << "Menu Asistente de posgrado.\n";
+                    cout << "1. Crear acta. \n2. Ver historicos\n 3. Salir\n";
+                    cin >> opcionMenuSecundario;
+                    switch (opcionMenuSecundario)
+                    {
+                    case 1:
 
-    } while (opcion != 0);
+                        break;
+                    case 2:
+                    
+                        break;
+                    case 3:
+                        break;
+                    }
+                }while (opcionMenuSecundario != 3);
+                break;
+            case 2:
+                do
+                {   //Menu secundario con las funcionalidades para ese rol
+                    cout << "Menu Director(a).\n";
+                    cout << "1. Modificar criterios. \n2. Ver historicos\n 3. Salir\n";
+                    cin >> opcionMenuSecundario;
+
+                    switch (opcionMenuSecundario)
+                    {
+                    case 1:
+
+                        break;
+                    case 2:
+                    
+                        break;
+                    case 3:
+                        break;
+                    }
+                }while (opcionMenuSecundario != 3);
+                break;
+            case 3:
+                do{ //Menu secundario con las funcionalidades para ese rol
+                    cout << "Menu Jurado(a).\n";
+                    cout << "1. Evaluar trabajo. \n2. Exportar acta.\n 3. Salir\n";
+                    cin >> opcionMenuSecundario;
+
+                    switch (opcionMenuSecundario)
+                    {
+                    case 1:
+
+                        break;
+                    case 2:
+                    
+                        break;
+                    case 3:
+                        break;
+                    }
+                }while (opcionMenuSecundario != 3);
+                break;
+        }
+    } while (opcionMenuPrincipal != 0);
 }
