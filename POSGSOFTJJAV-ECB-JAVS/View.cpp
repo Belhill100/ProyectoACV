f66634e4e9c95d1230146ea7353bae2a2063a01f
Belhill100 <ecollazosbravo@gmail.com>
2021-09-14 23:20:39 -0500

    Corregir View

--- POSGSOFTJJAV-ECB-JAVS/View.cpp
+++ POSGSOFTJJAV-ECB-JAVS/View.cpp
@@ -6,17 +6,18 @@ View::View(){
 
 void View::mostrarMenu(){
     int opcionMenuPrincipal, opcionMenuSecundario;
-    cout << "Menu\n";
-    cout << "Digite la opcion segun su tipo de usuario.\n";
-    cout << "1. Asistente de posgrado. \n2. Director(a). \n3. Jurado(a). \nOPC: ";
-    cin >> opcionMenuPrincipal;
+    
     do{
         // Menu principal para escoger el rol
+        cout << "Menu\n";
+        cout << "Digite la opcion segun su tipo de usuario.\n";
+        cout << "1. Asistente de posgrado. \n2. Director(a). \n3. Jurado(a).\n0. Salir \nOPC: ";
+        cin >> opcionMenuPrincipal;
         switch (opcionMenuPrincipal){
             case 1:
                 do{   //Menu secundario con las funcionalidades para ese rol
                     cout << "Menu Asistente de posgrado.\n";
-                    cout << "1. Crear acta. \n2. Ver historicos\n3. Salir\nOPC: ";
+                    cout << "1. Crear acta. \n2. Ver historicos\n0. Salir \nOPC: ";
                     cin >> opcionMenuSecundario;
                     switch (opcionMenuSecundario){
                     case 1:
@@ -25,13 +26,13 @@ void View::mostrarMenu(){
                     case 2:
                         break;
                     }
-                }while (opcionMenuSecundario != 3);
+                }while (opcionMenuSecundario != 0);
                 break;
             case 2:
                 do
                 {   //Menu secundario con las funcionalidades para ese rol
                     cout << "Menu Director(a).\n";
-                    cout << "1. Modificar criterios. \n2. Ver historicos\n3. Salir\n";
+                    cout << "1. Modificar criterios. \n2. Ver historicos\n0. Salir \n";
                     cin >> opcionMenuSecundario;
                     switch (opcionMenuSecundario){
                     case 1:
@@ -41,12 +42,12 @@ void View::mostrarMenu(){
 
                         break;
                     }
-                }while (opcionMenuSecundario != 3);
+                }while (opcionMenuSecundario != 0);
                 break;
             case 3:
                 do{ //Menu secundario con las funcionalidades para ese rol
                     cout << "Menu Jurado(a).\n";
-                    cout << "1. Evaluar trabajo. \n2. Exportar acta.\n0. Salir\n";
+                    cout << "1. Evaluar trabajo \n2. Exportar acta \n0. Salir \n";
                     cin >> opcionMenuSecundario;
                     switch (opcionMenuSecundario){
                     case 1:
